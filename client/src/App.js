44fa652f1f12dfd870fccdf227ee8af93a9ddbe8
Daisy Le <daisy.le0203@gmail.com>
2023-01-12 00:48:06 -0600

    import components to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from "@material-ui/core/CssBaseline"
+
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Hero from "./components/Hero/Hero"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <CssBaseline />
+      
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
