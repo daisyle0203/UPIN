bd7592d40f2d75998d34609530de578c081ef556
Daisy Le <daisy.le0203@gmail.com>
2023-01-12 21:21:01 -0600

    testing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category } = require('../models');
 const { signToken } = require('../utils/auth');
-
+//testing
 const resolvers = {
   Query: {
    
