8cc0d545193c1f97da44b57d03d18b0f4b0f037d
roodhouse <rughjm@gmail.com>
2023-01-12 21:16:57 -0600

    some starter on app.js and Login.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 import CssBaseline from "@material-ui/core/CssBaseline"
 
 import Header from "./components/Header/Header"
@@ -7,11 +10,18 @@ import Hero from "./components/Hero/Hero"
 function App() {
   return (
     <>
+    <Router>
     <CssBaseline />
-      
+      <Routes>
+        <Route 
+          path='/login'
+          element={<Login />}
+        />
+      </Routes>
       <Header />
       <Hero />
       <Footer />
+      </Router>
     </>
   )
 }
