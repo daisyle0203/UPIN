eb89c9bcaeca37997713c09027cbfa8685944d6a
Daisy Le <daisy.le0203@gmail.com>
2023-01-13 22:51:36 -0600

    add typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,98 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
+ type Review {
+  _id: ID!
+  company: Company!
+  user: User!
+  date: String!
+  interviewExperience: String
+  companyCulture: String
+  interviewer: String
+  overallExperience: Int
+  isAnonymous: Boolean
+  comments: [Comment]
+}
 
-`;
+type Company {
+  _id: ID!
+  name: String!
+  website: String!
+  location: String!
+  reviews: [Review]
+}
 
-module.exports = typeDefs;
+type User {
+  _id: ID!
+  email: String!
+  password: String!
+  reviews: [Review]
+  comments: [Comment]
+}
+
+type Comment {
+  _id: ID!
+  review: Review!
+  user: User!
+  comment: String!
+  date: String!
+}
+
+input ReviewInput {
+  company: ID!
+  user: ID!
+  date: String!
+  interviewExperience: String
+  companyCulture: String
+  interviewer: String
+  overallExperience: Int
+  isAnonymous: Boolean
+}
+
+input CompanyInput {
+  name: String!
+  website: String!
+  location: String!
+}
+
+input UserInput {
+  email: String!
+  password: String!
+}
+
+input CommentInput {
+  review: ID!
+  user: ID!
+  comment: String!
+  date: String!
+}
+
+type Query {
+  reviews: [Review]
+  review(id: ID!): Review
+  companies: [Company]
+  company(id: ID!): Company
+  users: [User]
+  user(id: ID!): User
+  comments: [Comment]
+  comment(id: ID!): Comment
+}
+
+type Mutation {
+  createReview(input: ReviewInput): Review
+  updateReview(id: ID!, input: ReviewInput): Review
+  deleteReview(id: ID!): Review
+  createCompany(input: CompanyInput): Company
+  updateCompany(id: ID!, input: CompanyInput): Company
+  deleteCompany(id: ID!): Company
+  createUser(input: UserInput): User
+  updateUser(id: ID!, input: UserInput): User
+  deleteUser(id: ID!): User
+  createComment(input: CommentInput): Comment
+  updateComment(id: ID!, input: CommentInput): Comment
+  deleteComment(id: ID!): Comment
+}
+
+`
+
+module.exports = typeDefs
