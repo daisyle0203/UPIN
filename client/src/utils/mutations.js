ec0a707016cc38bb3cb6b6afbb151d47e5f398f7
twangerwang <108110493+twangerwang@users.noreply.github.com>
2023-01-23 03:49:25 -0600

    added mutations and queries in front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,79 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_REVIEW = gql`
+  mutation AddReview(
+    $company: String!
+    $interviewExperience: String!
+    $role: String!
+    $interviewerInfo: String!
+    $rating: Int!
+  ) {
+    addReview(
+      company: $company
+      interviewExperience: $interviewExperience
+      role: $role
+      interviewerInfo: $interviewerInfo
+      rating: $rating
+    ) {
+      _id
+      company
+      role
+      interviewExperience
+      interviewerInfo
+      rating
+      reviewAuthor
+      createdAt
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation AddComment($reviewId: ID!, $commentText: String!) {
+    addComment(reviewId: $reviewId, commentText: $commentText) {
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+export const REMOVE_REVIEW = gql`
+  mutation Mutation($reviewId: ID!) {
+    removeReview(reviewId: $reviewId) {
+      _id
+      company
+      interviewExperience
+      role
+      interviewerInfo
+      reviewAuthor
+      rating
+      createdAt
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation Mutation($reviewId: ID!, $commentId: ID!) {
+    removeComment(reviewId: $reviewId, commentId: $commentId) {
+      _id
+      company
+      interviewExperience
+      role
+      interviewerInfo
+      reviewAuthor
+      rating
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
